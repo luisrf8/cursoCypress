4d326a8aed99c565b25e3deebe5e500e6d71be55
luisrf8 <larojasf1999@gmail.com>
2023-01-30 09:11:00 -0400

    capitulo 15

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -127,3 +127,57 @@
   //   })
   // })
 // End Validar los checkbox
+
+// Para filtrar por un select 
+  // it ("Order by highest to lowest", () => {
+  //   cy.get("element,#id,.class,etc").select("value")
+  //   //en el select value vamos a indicar el valor del filtro que querramos buscar buscandolo con el inspeccionador de la interfaz de cypress
+  //   //value es sesitive, tiene que ser escrito tal cual como esta en el inspeccionador 
+  // })
+// End Para filtrar por un select 
+
+// si tengo mas de un elemento con el mismo nombre de seleccion o la misma clase y quiero usar el primer elemento
+  // describe("go to different sections", () => {
+  //   it("go to first section", () => {
+  //     // antes del .click() se coloca la posicion a la que queremos darle click 
+  //     cy.get(["class=example"]).first().click();
+  //   })
+  //   it("go to second section", () => {
+  //     // antes del .click() se coloca la posicion en caso de ser un arreglo colocando eq y dentro de los parentesis la posicion del elemento en el arreglo a la que queremos darle click 
+  //     cy.get(["class=example"]).eq(2).click();
+  //   })
+  // })
+// End si tengo mas de un elemento con el mismo nombre de seleccion o la misma clase y quiero usar el primer elemento
+
+// asserts
+// describe("cuentas", () => {
+//   var a=1;
+//   it('vamos a ver una igualdad', () => {
+//     expect(1==1, "aqui podemos explicar de que va a ser el assert y que posible respuesta esperamos si falla").to.equal(true);
+//   })
+//   it('vamos a ver que no sea una igualdad', () => {
+//     expect(1==2).to.equal(true);
+//   })
+//   it('vamos a ver que una resta este bien', () => {
+//     expect(1-5, "aqui se espera un 0").to.equal(0);
+//   })
+//   it('vamos a ver que una resta este mal', () => {
+//     expect(1-1).to.not.equal(2);
+//   })
+//   it('tiene que ser verdadero', () => {
+//     expect(2==2).to.be.true;
+//   })
+//   it('tiene que ser falso', () => {
+//     expect(2==1).to.be.false;
+//   })
+//   it("la variable existe", () => {
+//     expect(a).to.exist;
+//   })
+//   it("es menor a 10"), () => {
+//     expect(a).to.be.lessThan(10);
+//   }
+//   it("es mayor a 10"), () => {
+//     expect(a).to.be.greaterThan(10);
+//   }
+// })
+// end asserts 
