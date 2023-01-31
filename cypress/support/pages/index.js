73070c8d14fe17f989caa6d1153f343a6b071145
luisrf8 <larojasf1999@gmail.com>
2023-01-31 08:49:05 -0400

    locators

--- cypress/support/pages/index.js
+++ cypress/support/pages/index.js
@@ -1,12 +1,24 @@
   class indexPage{
-    constructor() {
-      this.searchInput = "#id del componente a buscar en la pagina"
-      this.searchButton = "#id del boton > .clase del boton"
-    }
+    // Aqui trabajamos con la paginacion, creando componentes con page Objects para luego llamarlos donde los necesitemos
+    // constructor() {
+    //   this.searchInput = "#id del componente a buscar en la pagina"
+    //   this.searchButton = "#id del boton > .clase del boton"
+    // }
+    // search = (element) => {
+    //   cy.get(this.searchInput).type(element);
+    //   cy.get(this.searchButton).click();
+    // }
+    //End  Aqui trabajamos con la paginacion, creando componentes con page Objects para luego llamarlos donde los necesitemos
+
+    //aqui usamos el fixture locators creando el json index.json en la carpeta fixtures
     search = (element) => {
-      cy.get(this.searchInput).type(element);
-      cy.get(this.searchButton).click();
-    }
+        cy.fixture('index.json').then((locators) => {
+        cy.get(locators.searchInput).type(element);
+        cy.get(locators.searchButton).click();
+        })
+      }
+    //end aqui usamos el fixture locators creando el json index.json en la carpeta fixtures
+    
   }
 
   export default new indexPage();
